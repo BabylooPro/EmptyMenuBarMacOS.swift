83146138776df56dc21d16528ab7bbabba1b339a
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-10-06 23:13:44 +0200

    Initial Commit

---
+++ EmptyMenuBar/EmptyMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyMenuBarApp.swift
+//  EmptyMenuBar
+//
+//  Created by Max Remy on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmptyMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmptyMenuBarTests/EmptyMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyMenuBarTests.swift
+//  EmptyMenuBarTests
+//
+//  Created by Max Remy on 06.10.2024.
+//
+
+import Testing
+@testable import EmptyMenuBar
+
+struct EmptyMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
